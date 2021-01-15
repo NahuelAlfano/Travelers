dbec5116a2f80b55e5d42368a702ebe44326eacc
Nahuel-dev <nahualfano@gmail.com>
2021-01-14 22:15:24 -0300

    agregado las api y seccion de clima

---
+++ script.js
@@ -0,0 +1,10 @@
+// API DE FOURSQUARE INFO
+
+const clientId = 'WJFXTRDQPPM5W4M514W5COWMLXHGL3FCBYKKCKRTO1UFRGWU';
+const clientSecret = 'OCDR0W2TPPDLIEENXCUTAEUQYLKLBTPROGMJXNNG5J0N5VSD';
+const url = 'https://api.foursquare.com/v2/venues/explore?near=';
+
+// API DE OPENWEATHER INFO
+
+const openWeatherKey = '6ffaca22cba7d7f80e932c7b260f260f';
+const weatherUrl = 'https://api.openweathermap.org/data/2.5/weather';
